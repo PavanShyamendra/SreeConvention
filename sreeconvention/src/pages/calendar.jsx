e81641a808edafa08d7cbc08a3e52e7aec3ca75b
PavanShyamendra <n.p.shyamendra3322@gmail.com>
2022-07-20 18:31:51 +0530

    add calendar framework

--- sreeconvention/src/pages/calendar.jsx
+++ sreeconvention/src/pages/calendar.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
-import {useState} from 'react';
-import Calendar from 'react-calendar'; 
+import { useCallback, useState } from 'react';
+import { Calendar } from '@natscale/react-calendar';
+import '@natscale/react-calendar/dist/main.css';
+
 
 const CalendarPage = () => {
 
-    const [date, setDate] = useState(new Date())
-  return (
-    <div className="bg-main min-h-screen">
+    const [value, setValue] = useState(new Date());
 
-      <div className="flex justify-center p-4">
-        <Calendar onChange={setDate} value={date} className="justify-center bg-main"/>
+    const onChange = useCallback(
+      (val) => {
+        setValue(val);
+      },
+      [setValue],
+    );
+    const allDates = new Set(["Tue Jun 21 2022","Mon Jun 27 2022","Fri Jul 01 2022"]);
+
+    const isHighlight = useCallback((date) => {
+      // highlight any data that is divisible by 5
+      console.log(date.toDateString())
+      if (allDates.has(date.toDateString())) {
+        return true;
+      }
+    }, []);
+
+    return (
+      <div className='bg-main flex items-start justify-center min-h-screen ' >
+        <Calendar isHighlight={isHighlight} size={500} fontSize = {30} value={value} onChange={onChange} className = 'p-4 mt-20' />
       </div>
-    </div>
-  )
+    )
 }
 
 export default CalendarPage
