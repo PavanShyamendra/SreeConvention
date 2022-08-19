0bf965ff37edfa6511ae61dac825c42e546e5be5
PavanShyamendra <n.p.shyamendra3322@gmail.com>
2022-08-19 19:12:47 +0530

    try build

--- sreeconvention/src/App.jsx
+++ sreeconvention/src/App.jsx
@@ -1,20 +1,19 @@
-import './App.css'
-import React from 'react'
+import "./App.css";
+import React from "react";
 
-import { Routes, Route } from 'react-router-dom'
+// import { Routes, Route } from 'react-router-dom'
 
-import HomeScreen from './pages/home'
-import CalendarPage from './pages/calendar'
-
+import HomeScreen from "./pages/home";
+import CalendarPage from "./pages/calendar";
 
 export default function App() {
   return (
     <>
-      { <Routes>
+      {/* { <Routes>
         <Route exact element = {<HomeScreen/>} path ="/"/>
         <Route element={<CalendarPage />} path="/calendar" />
-      </Routes>}
-      
-      </>
-  )
+      </Routes>} */}
+      <HomeScreen />
+    </>
+  );
 }
